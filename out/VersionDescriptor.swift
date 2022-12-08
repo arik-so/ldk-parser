899626f9ca62ceb255d1dbe1e68a4b3b85b0df62
Arik Sosman <git@arik.io>
2022-12-07 18:38:58 -0800

    update version

--- out/VersionDescriptor.swift
+++ out/VersionDescriptor.swift
@@ -4,14 +4,14 @@
 			#endif
 
 			extension Bindings {
-				public class func getLDKSwiftBindingsSerializationHash(): String {
-					return "36f21656e881b5572e597cfa6d7da9a68c22f243680f9c4e99d12d363a85975b"
+				public class func getLDKSwiftBindingsSerializationHash() -> String {
+					return "e80e1bae00d8e97a2924b4628e3c747ad3ff94260ca05acee1e3cd28717e0337"
 				}
-				public class func getLDKSwiftBindingsVersion(): String {
-					return "5f5d773"
+				public class func getLDKSwiftBindingsVersion() -> String {
+					return "d108ff0"
 				}
-				public class func getLDKSwiftBindingsCommitHash(): String {
-					return "5f5d77332ec8bcc1a5913589d426bcee58e4530f"
+				public class func getLDKSwiftBindingsCommitHash() -> String {
+					return "d108ff00c6bc272aabc2ebfd8ecd030f8635b834"
 				}
 			}
 		
